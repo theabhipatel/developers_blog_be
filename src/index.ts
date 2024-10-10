a82119ea07d81f693b97648dd213ebd3c4c43b6e
theabhipatel <theabhipatel.co@gmail.com>
2024-10-10 12:53:38 +0530

    feat setup a basic express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("I am live ........");
+import express from "express";
+
+const app = express();
+
+const PORT = 3331;
+const HOST_NAME = "127.0.0.1";
+
+/** ---> Registering middlewares. */
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true }));
+
+/** ---> Handling home route. */
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to the Developers Blog." });
+});
+
+/** ---> Handling not found (404) routes. */
+app.use("*", (req, res) => {
+  res.status(404).json({ success: false, message: "Route not found." });
+});
+
+app.listen(Number(PORT), HOST_NAME, () => {
+  console.log(`[::] Server is running at http://${HOST_NAME}:${PORT}`);
+});
