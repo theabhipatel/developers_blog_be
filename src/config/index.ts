c8c3e1e0871bb46e5625f52ca98237069fe6f670
theabhipatel <theabhipatel.co@gmail.com>
2024-10-11 14:14:51 +0530

    add jwt secret variable in config and .env.example file

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,3 +8,6 @@ export const HOST_NAME = process.env.HOST_NAME || "127.0.0.1";
 
 /** ---> Database credentials. */
 export const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://127.0.0.1:27017/developersblog";
+
+/** ---> jwt secrets. */
+export const JWT_SECRET = process.env.JWT_SECRET!;
