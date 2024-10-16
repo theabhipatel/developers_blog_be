00faf121a562aa49870db1d937ce67ceb8a5a98a
theabhipatel <theabhipatel.co@gmail.com>
2024-10-16 18:59:31 +0530

    feat created an oauth sigin handler for social login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -85,27 +85,53 @@ export const signinHandler: RequestHandler = async (req, res, next) => {
 
 export const oAuthSigninHandler: RequestHandler = async (req, res, next) => {
   try {
-    // const { email, password } = req.body;
-    console.log("oauth req.body --->", req.body); // eslint-disable-line
+    const { email, firstName, lastName, profilePic, provider } = req.body;
 
-    // const user = await userModel.findOne({ email });
-    // if (!user) return res.status(401).json({ success: false, message: "Invalid credantials." });
-    // if (!user.isVerified)
-    //   return res.status(401).json({ success: false, message: "Email not verified." });
-    // if (user.isBlocked)
-    //   return res.status(401).json({ success: false, message: "User is blocked." });
-    // if (user.isDeleted)
-    //   return res.status(403).json({ success: false, message: "This account is deleted." });
+    const user = await userModel.findOne({ email });
+    if (user) {
+      if (user.isBlocked) {
+        res.status(401).json({ success: false, message: "User is blocked." });
+        return;
+      }
+      if (user.isDeleted) {
+        res.status(403).json({ success: false, message: "This account is deleted." });
+        return;
+      }
+      const userProfile = await userProfileModel
+        .findOne({ user: user._id })
+        .select("firstName lastName profilePic")
+        .lean();
 
-    // const isPasswordMatched = await bcrypt.compare(password, user.password);
-    // if (!isPasswordMatched)
-    //   return res.status(401).json({ success: false, message: "Invalid credantials." });
+      const accessToken = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET);
 
-    // const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET);
+      res.status(200).json({
+        success: true,
+        message: "User logged in successfully.",
+        user: { ...userProfile, email, role: user.role, _id: user._id },
+        accessToken,
+      });
+    } else {
+      const newUser = await userModel.create({
+        email,
+        provider,
+        isVerified: true,
+      });
+      await userProfileModel.create({
+        user: newUser._id,
+        firstName,
+        lastName,
+        profilePic,
+      });
 
-    // res.status(200).json({ success: true, message: "User logged in successfully.", token });
+      const accessToken = jwt.sign({ userId: newUser._id, role: newUser.role }, JWT_SECRET);
 
-    res.status(200).json({ success: true, message: "User logged in successfully." });
+      res.status(200).json({
+        success: true,
+        message: "User logged in successfully.",
+        user: { _id: newUser._id, firstName, lastName, profilePic, email, role: newUser.role },
+        accessToken,
+      });
+    }
   } catch (error) {
     next(error);
   }
