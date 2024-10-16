a9f04c83a7c682b99dafb5cc1da351163bfb89be
theabhipatel <theabhipatel.co@gmail.com>
2024-10-16 19:07:52 +0530

    fix typescript return type error in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,12 +6,14 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@/config";
 import { sendEMail } from "@/utils/sendEMail";
 
-// @ts-expect-error [.] will fix type error later
 export const singupHandler: RequestHandler = async (req, res, next) => {
   try {
     const { firstName, lastName, email, password } = req.body;
     const user = await userModel.findOne({ email });
-    if (user) return res.status(403).json({ success: false, message: "User already registered." });
+    if (user) {
+      res.status(403).json({ success: false, message: "User already registered." });
+      return;
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -37,7 +39,7 @@ export const singupHandler: RequestHandler = async (req, res, next) => {
       html: `<h1>Hello ${firstName}, </h1> <p> please verify your email. <br/> this is verification token : ${token} </p>`,
     });
 
-    return res.status(201).json({
+    res.status(201).json({
       success: true,
       message: "User registered successfully, Please verify your email.",
       testMessageUrl,
@@ -47,23 +49,33 @@ export const singupHandler: RequestHandler = async (req, res, next) => {
   }
 };
 
-// @ts-expect-error [.] will fix type error later
 export const signinHandler: RequestHandler = async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
     const user = await userModel.findOne({ email });
-    if (!user) return res.status(401).json({ success: false, message: "Invalid credantials." });
-    if (!user.isVerified)
-      return res.status(401).json({ success: false, message: "Email not verified." });
-    if (user.isBlocked)
-      return res.status(401).json({ success: false, message: "User is blocked." });
-    if (user.isDeleted)
-      return res.status(403).json({ success: false, message: "This account is deleted." });
+    if (!user) {
+      res.status(401).json({ success: false, message: "Invalid credantials." });
+      return;
+    }
+    if (!user.isVerified) {
+      res.status(401).json({ success: false, message: "Email not verified." });
+      return;
+    }
+    if (user.isBlocked) {
+      res.status(401).json({ success: false, message: "User is blocked." });
+      return;
+    }
+    if (user.isDeleted) {
+      res.status(403).json({ success: false, message: "This account is deleted." });
+      return;
+    }
 
     const isPasswordMatched = await bcrypt.compare(password, user.password);
-    if (!isPasswordMatched)
-      return res.status(401).json({ success: false, message: "Invalid credantials." });
+    if (!isPasswordMatched) {
+      res.status(401).json({ success: false, message: "Invalid credantials." });
+      return;
+    }
 
     const userProfile = await userProfileModel
       .findOne({ user: user._id })
