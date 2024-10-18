ec924b267ef798c23a0f79fddb898b12c0b2778e
theabhipatel <theabhipatel.co@gmail.com>
2024-10-18 17:57:15 +0530

    chore added authorize middleware in add and update blog routes

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -4,13 +4,15 @@ import {
   getBlogHandler,
   updateBlogHandler,
 } from "@/controllers/blog.controller";
+import { authorize } from "@/middlewares/authorize";
+import { ERoles } from "@/models/user.model";
 import { Router } from "express";
 
 const blogRouter = Router();
 
 blogRouter.get("/", getAllBlogHandler);
 blogRouter.get("/:blogId", getBlogHandler);
-blogRouter.post("/add", addBlogHandler);
-blogRouter.patch("/update/:blogId", updateBlogHandler);
+blogRouter.post("/add", authorize([ERoles.ADMIN, ERoles.USER]), addBlogHandler);
+blogRouter.patch("/update/:blogId", authorize([ERoles.ADMIN, ERoles.USER]), updateBlogHandler);
 
 export default blogRouter;
