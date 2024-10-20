b954020ff69d06fc38855736970780b112123f0b
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-20 14:37:12 +0530

    chore used validate middleware to validate add blog request input in blog.routes.ts

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -5,14 +5,21 @@ import {
   updateBlogHandler,
 } from "@/controllers/blog.controller";
 import { authorize } from "@/middlewares/authorize";
+import { validate } from "@/middlewares/validate";
 import { ERoles } from "@/models/user.model";
+import { addBlogSchema } from "@/validation/blog";
 import { Router } from "express";
 
 const blogRouter = Router();
 
 blogRouter.get("/", getAllBlogHandler);
 blogRouter.get("/:blogId", getBlogHandler);
-blogRouter.post("/add", authorize([ERoles.ADMIN, ERoles.USER]), addBlogHandler);
+blogRouter.post(
+  "/add",
+  authorize([ERoles.ADMIN, ERoles.USER]),
+  validate(addBlogSchema),
+  addBlogHandler
+);
 blogRouter.patch("/update/:blogId", authorize([ERoles.ADMIN, ERoles.USER]), updateBlogHandler);
 
 export default blogRouter;
