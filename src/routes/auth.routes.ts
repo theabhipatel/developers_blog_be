305b1564b49316333dfb2d1fb3c2e6f7c75ffba5
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-20 15:57:12 +0530

    add validation schema for oAuthSigninHandler and validate with it in auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import { oAuthSigninHandler, signinHandler, singupHandler } from "@/controllers/auth.controller";
 import { validate } from "@/middlewares/validate";
-import { signinSchema, singupSchema } from "@/validation/auth";
+import { oAuthSigninSchema, signinSchema, singupSchema } from "@/validation/auth";
 import { Router } from "express";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validate(singupSchema), singupHandler);
 authRouter.post("/signin", validate(signinSchema), signinHandler);
-authRouter.post("/oauth-signin", oAuthSigninHandler);
+authRouter.post("/oauth-signin", validate(oAuthSigninSchema), oAuthSigninHandler);
 
 export default authRouter;
--- src/validation/auth.ts
+++ src/validation/auth.ts
@@ -1,4 +1,7 @@
-import { object, string } from "zod";
+import { EProvider } from "@/models/user.model";
+import { object, string, enum as enum_ } from "zod";
+
+const providerEnum = Object.values(EProvider) as [EProvider, ...EProvider[]];
 
 export const singupSchema = object({
   body: object({
@@ -16,5 +19,19 @@ export const signinSchema = object({
   body: object({
     email: string({ required_error: "email is required." }).email(),
     password: string({ required_error: "password is required." }),
+  }),
+});
+
+export const oAuthSigninSchema = object({
+  body: object({
+    firstName: string({ required_error: "firstName is required." }),
+    lastName: string({ required_error: "lastName is required." }),
+    email: string({ required_error: "email is required." }).email(),
+    profilePic: string({ required_error: "profilePic is required." }).url({
+      message: "profilePic must be a url",
+    }),
+    provider: enum_(providerEnum, {
+      required_error: "provider is required.",
+    }),
   }),
 });
