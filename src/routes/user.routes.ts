34297306b80dd4db79d9d6b10d85b28884591c65
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-27 21:33:40 +0530

    feat added authorization middleware in follow and un follow routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,19 @@ import {
   getUserProfileByUserNameHandler,
   unFollowUserHandler,
 } from "@/controllers/user.controller";
+import { authorize } from "@/middlewares/authorize";
+import { ERoles } from "@/models/user.model";
 import { Router } from "express";
 
 const userRouter = Router();
 
 // [] TODO : Have to create request validation schema
 userRouter.get("/profile/:username", getUserProfileByUserNameHandler);
-userRouter.post("/follow/:followingId", followUserHandler);
-userRouter.delete("/unfollow/:followingId", unFollowUserHandler);
+userRouter.post("/follow/:followingId", authorize([ERoles.ADMIN, ERoles.USER]), followUserHandler);
+userRouter.delete(
+  "/unfollow/:followingId",
+  authorize([ERoles.ADMIN, ERoles.USER]),
+  unFollowUserHandler
+);
 
 export default userRouter;
