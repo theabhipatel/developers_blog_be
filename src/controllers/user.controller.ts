2a987b44ac26df3e0ebe13c2579f6184bae69eb3
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-27 14:48:23 +0530

    feat created handler for get user profile by username

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,18 @@
 import userModel from "@/models/user.model";
+import userProfileModel from "@/models/userProfile.model";
 import { RequestHandler } from "express";
 
 export const getUserProfileByUserName: RequestHandler = async (req, res, next) => {
   try {
     const username = req.params.username;
-    const profile = await userModel.findOne({ username });
+    const user = await userModel.findOne({ username });
+    if (!user) {
+      res.status(404).json({ success: false, message: "User not found." });
+      return;
+    }
+    const profile = await userProfileModel.findOne({ user: user?._id });
 
-    res.status(201).json({
+    res.status(200).json({
       success: true,
       message: "Profile fetched successfully.",
       profile,
