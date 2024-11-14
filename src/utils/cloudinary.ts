1414139870f8af1ff8b97f9a697744b8855ed4a3
theabhipatel <theabhipatel.co@gmail.com>
2024-11-14 19:56:30 +0530

    create a cloudinary config function with credentials to use in application

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,9 @@
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } from "@/config";
+import { v2 as cloudinaryConfig } from "cloudinary";
+
+export const cloudinary = cloudinaryConfig.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
