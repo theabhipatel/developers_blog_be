76a74b7e42af1e2037dc571bb3bb5e68c0525062
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-11-16 16:24:21 +0530

    feat added search and pagination functionality in get all blogs handler

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -57,8 +57,22 @@ export const updateBlogHandler: RequestHandler = async (req, res, next) => {
 
 export const getAllBlogsHandler: RequestHandler = async (req, res, next) => {
   try {
+    const { search } = req.query;
+    interface IQuery {
+      title?: unknown;
+    }
+    const query: IQuery = {};
+    if (search) {
+      query.title = { $regex: search, $options: "i" };
+    }
+
+    /** ---> Pagination logic */
+    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+
     const blogs = await blogModel
-      .find({})
+      .find(query)
       .select("-content")
       .populate({
         path: "user",
@@ -69,7 +83,11 @@ export const getAllBlogsHandler: RequestHandler = async (req, res, next) => {
           select: "firstName lastName profilePic",
         },
       })
+      .skip(skip)
+      .limit(limit)
       .lean();
+
+    const totalCount = await blogModel.find(query).countDocuments();
 
     /*  eslint-disable */
     const transformedBlogs = (blogs as any[]).map((blog: IBlog) => {
@@ -92,6 +110,12 @@ export const getAllBlogsHandler: RequestHandler = async (req, res, next) => {
       success: true,
       message: "Blogs fetched successfully.",
       blogs: transformedBlogs,
+      meta: {
+        total: totalCount,
+        page,
+        limit,
+        totalPages: Math.ceil(totalCount / limit),
+      },
     });
   } catch (error) {
     next(error);
