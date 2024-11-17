87cde69ed52fdab787d93b8d268eff73ce5cc9fc
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-11-17 16:46:50 +0530

    chore added default blogs per page limit to 24 in get all blog handler

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -73,7 +73,7 @@ export const getAllBlogsHandler: RequestHandler = async (req, res, next) => {
     }
 
     /** ---> Pagination logic */
-    const limit = Number(req.query.limit) || 10;
+    const limit = Number(req.query.limit) || 24;
     const page = Number(req.query.page) || 1;
     const skip = (page - 1) * limit;
 
