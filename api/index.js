ba2bd4803e98d2ef4a3a3eaea3ebe03e53f0c5f6
theabhipatel <theabhipatel.co@gmail.com>
2026-02-07 21:06:54 +0530

    chore: update vercel.json file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 /**
  * Vercel deploy entry handler, for serverless deployment, please don't modify this file
  */
-import app from "../dist/app";
+import app from "../dist/app.js";
 
 module.exports = (req, res) => {
   return app(req, res);
